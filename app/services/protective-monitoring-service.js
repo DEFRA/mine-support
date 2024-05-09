a039b6530fcb89d475a1637112b18930d98a2d46
Aimee Horwood <aimee.horwood@capgemini.com>
2024-05-09 14:16:45 +0100

    bug fix for user identification http

--- app/services/protective-monitoring-service.js
+++ app/services/protective-monitoring-service.js
@@ -1,33 +1,43 @@
-const { PublishEvent } = require('ffc-protective-monitoring')
-const config = require('../config')
+const { PublishEvent } = require("ffc-protective-monitoring");
+const config = require("../config");
 
-async function sendEvent (request, claim, event) {
-  const protectiveMonitoring = new PublishEvent(config.protectiveMonitoringUrl)
+async function sendEvent(request, claim, event) {
+  const protectiveMonitoring = new PublishEvent(config.protectiveMonitoringUrl);
   await protectiveMonitoring.sendEvent({
     sessionid: claim.claimId,
     datetime: createEventDate(),
-    version: '1.1',
-    application: 'ffc-demo-web',
-    component: 'ffc-demo-web',
+    version: "1.1",
+    application: "ffc-demo-web",
+    component: "ffc-demo-web",
     ip: getIpAddress(request),
-    pmccode: '001',
-    priority: '0',
+    pmccode: "001",
+    priority: "0",
     details: {
-      message: event
-    }
-  })
-  console.log(`Protective monitoring event sent: ${event}`)
+      message: event,
+    },
+  });
+  console.log(`Protective monitoring event sent: ${event}`);
 }
 
-function getIpAddress (request) {
-  // Identifying the originating IP address of a client connecting to a web server through an HTTP proxy or a load balancer
-  const xForwardedForHeader = request.headers['x-forwarded-for']
-  return xForwardedForHeader ? xForwardedForHeader.split(',')[0] : request.info.remoteAddress
+function getIpAddress(request) {
+  const headerName = process.env.IP_FORWARD_HEADER_NAME;
+
+  // Checks if environment variable is defined for header name otherwise return the direct IP address
+
+  if (!headerName) {
+    return request.info.remoteAddress;
+  }
+
+  const xForwardedForHeader = request.headers[headerName];
+
+  return xForwardedForHeader
+    ? xForwardedForHeader.split(",")[0]
+    : request.info.remoteAddress;
 }
 
-function createEventDate () {
-  const eventDate = new Date()
-  return eventDate.toISOString()
+function createEventDate() {
+  const eventDate = new Date();
+  return eventDate.toISOString();
 }
 
-module.exports = sendEvent
+module.exports = sendEvent;
