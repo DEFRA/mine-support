db052e9c941bd577f61b15b956e5fd5d64be50ac
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2024-05-24 13:57:30 +0100

    lint fix

--- app/services/app-insights.js
+++ app/services/app-insights.js
@@ -1,18 +1,16 @@
-const { metrics, trace } = require("@opentelemetry/api");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { ExpressInstrumentation } = require("@opentelemetry/instrumentation-express");
-const { useAzureMonitor } = require("@azure/monitor-opentelemetry");
-const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
-const { Resource } = require( '@opentelemetry/resources');
+const { metrics, trace } = require('@opentelemetry/api')
+const { registerInstrumentations } = require('@opentelemetry/instrumentation')
+const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express')
+const { useAzureMonitor } = require('@azure/monitor-opentelemetry')
+const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions')
+const { Resource } = require('@opentelemetry/resources')
 const resource = new Resource({
-  [SEMRESATTRS_SERVICE_NAME]: process.env.APPINSIGHTS_CLOUDROLE,
-});
-const { DefaultAzureCredential } = require( "@azure/identity");
+  [SEMRESATTRS_SERVICE_NAME]: process.env.APPINSIGHTS_CLOUDROLE
+})
+const { DefaultAzureCredential } = require('@azure/identity')
 
 function setup () {
-
-  if (process.env.APPINSIGHTS_CONNECTIONSTRING) {
-    
+  if (process.env.APPINSIGHTS_CONNECTIONSTRING) {    
     const options = {
       azureMonitorExporterOptions: {
         connectionString:
@@ -23,18 +21,16 @@ function setup () {
     }
     useAzureMonitor(options);
     const instrumentations = [
-      new ExpressInstrumentation(),
+      new ExpressInstrumentation()
     ];
     registerInstrumentations({
       tracerProvider: trace.getTracerProvider(),
       meterProvider: metrics.getMeterProvider(),
-      instrumentations: instrumentations,
+      instrumentations: instrumentations
     });  
     console.log('App Insights Running')
   } else {
     console.log('App Insights Not Running!')
   }
-
 }
-
 module.exports = { setup }
--- test/unit/app-insights.test.js
+++ test/unit/app-insights.test.js
@@ -1,42 +1,37 @@
 const applicationName = 'test-app'
 
-jest.mock("@azure/monitor-opentelemetry", () => ({
-  useAzureMonitor: jest.fn(),
-}));
-jest.mock("@opentelemetry/resources");
-jest.mock("@azure/identity");
-const { useAzureMonitor } = require("@azure/monitor-opentelemetry");
-describe("openTelemetry", () => {
+jest.mock('@azure/monitor-opentelemetry', () => ({
+  useAzureMonitor: jest.fn()
+}))
+jest.mock('@opentelemetry/resources')
+jest.mock('@azure/identity')
+const { useAzureMonitor } = require('@azure/monitor-opentelemetry')
+describe('openTelemetry', () => {
   beforeEach(() => {
     process.env.APPINSIGHTS_CLOUDROLE = applicationName
-    jest.clearAllMocks();
-  });
+    jest.clearAllMocks()
+  })
 
-  it("should call useAzureMonitor with the correct ", () => {
-    process.env.APPINSIGHTS_CONNECTIONSTRING = "test-connection-string";
-    process.env.NODE_ENV = "production";
+  it('should call useAzureMonitor with the correct ', () => {
+    process.env.APPINSIGHTS_CONNECTIONSTRING = 'test-connection-string'
+    process.env.NODE_ENV = 'production'
+    const { setup } = require('../../app/services/app-insights.js')
+    setup()
+    expect(useAzureMonitor).toHaveBeenCalled()
+  })
 
-    const { setup } = require("../../app/services/app-insights.js");
-    setup();
-    expect(useAzureMonitor).toHaveBeenCalled();
-  });
+  it('should not call useAzureMonitor if SHARED_APPINSIGHT_CONNECTIONSTRING is not set', () => {
+    process.env.APPINSIGHTS_CONNECTIONSTRING = ''
+    const { setup } = require('../../app/services/app-insights.js')
+    setup()
+    expect(useAzureMonitor).not.toHaveBeenCalled()
+  })
 
-  it("should not call useAzureMonitor if SHARED_APPINSIGHT_CONNECTIONSTRING is not set", () => {
-    process.env.APPINSIGHTS_CONNECTIONSTRING = "";
-
-    const { setup } = require("../../app/services/app-insights.js");
-    setup();
-    expect(useAzureMonitor).not.toHaveBeenCalled();
-  });
-
-  it("should not pass credential if NODE_ENV is 'development'", () => {
-    process.env.APPINSIGHTS_CONNECTIONSTRING = "test-connection-string";
-    process.env.NODE_ENV = "development";
-
-    const { setup } = require("../../app/services/app-insights.js");
-    setup();
-
-    expect(useAzureMonitor).toHaveBeenCalled();
-  });
-});
-
+  it('should not pass credential if NODE_ENV is development', () => {
+    process.env.APPINSIGHTS_CONNECTIONSTRING = 'test-connection-string'
+    process.env.NODE_ENV = 'development'
+    const { setup } = require('../../app/services/app-insights.js')
+    setup()
+    expect(useAzureMonitor).toHaveBeenCalled()
+  })
+})
