ad1e9296b5a3e6544e8d9197237f816eb579b022
dsanabriaf <daniel@naso.tech>
2024-10-21 20:47:13 +0100

    Fixing linting

--- test/contract/debug.test.js
+++ test/contract/debug.test.js
@@ -1,10 +1,10 @@
 // test.js
-const assert = require('assert');
+const assert = require('assert')
 
-describe('Environment Variable Test', function() {
-  it('should print the environment variable', function() {
-    const envVar = process.env.MY_ENV_VAR;
-    console.log('Environment Variable:', envVar);
-    assert.ok(envVar, 'Environment variable is not set');
-  });
-});
+describe('Environment Variable Test', function () {
+  it('should print the environment variable', function () {
+    const envVar = process.env.MY_ENV_VAR
+    console.log('Environment Variable:', envVar)
+    assert.ok(envVar, 'Environment variable is not set')
+  })
+})
