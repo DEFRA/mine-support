b29fe68bb4f706726d43584dd75eacb34f39d4c8
dsanabriaf <daniel@naso.tech>
2024-10-22 08:09:52 +0100

    Fixing typos

--- test/contract/debug.test.js
+++ test/contract/debug.test.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 
 describe('Environment Variable Test', function () {
   it('should print the environment variable', function () {
-    const envVar = process.env.ffc-demo-web-TEST-VAR
+    const envVar = process.env.ffc_demo_web_TEST_VAR
     console.log('Environment Variable:', envVar)
     assert.ok(envVar, 'Environment variable is not set')
   })
